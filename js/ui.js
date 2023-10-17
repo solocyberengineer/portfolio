dc4b2d423aaf71d9558b60f269cd1235974b8b00
solocyberengineer <solocyberengineer@gmail.com>
2023-10-17 06:17:46 +0200

    added navigation to home page

---
+++ js/ui.js
@@ -0,0 +1,7 @@
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+var homeSection = document.getElementById('home');
+
+homeSection.style['width'] = `${windowWidth.toString()}px`;
+homeSection.style['height'] = `${windowHeight.toString()}px`;
